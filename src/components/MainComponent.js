868e29433b864b0bb98b0872b2b8ef25681be284
manichekuri99 <chekuri.balamanikanta@iiitb.org>
2020-11-12 13:55:48 +0530

    3 files modified

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Pannel from './PannelComponent';
 
 
 class Main extends Component {
@@ -6,8 +7,9 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <div className = "container">
-        </div>
+        <Pannel />
+        <Pannel />
+        <Pannel />
       </div>
     );
   }
--- src/components/PannelComponent.js
+++ src/components/PannelComponent.js
@@ -3,9 +3,12 @@ import React, { Component } from "react";
 class Pannel extends Component {
   render() {
     return(
-      <div>
-        <p></p>
+      <div className = "container">
+        <h1>City</h1>
+        <p>Weather Report</p>
       </div>
     ); 
   }
 }
+
+export default Pannel;
